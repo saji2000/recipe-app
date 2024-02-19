f06f81c72c50b601839713d025bf966f859c8ab3
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-19 12:10:49 -0500

    Started working on registering the users

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import { userRouter } from "./routes/users.js";
+
 dotenv.config();
 
 const password = process.env.PASSWORD;
@@ -12,10 +14,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/auth", userRouter);
+
 mongoose.connect(
   `mongodb+srv://hashkim21:${password}@recipe-app.xkipg0g.mongodb.net/recipe-app?retryWrites=true&w=majority`
 );
 
-app.listen(3001, () => {
-  console.log("listening on port 3001");
+app.listen(3003, () => {
+  console.log("listening on port 3003");
 });
