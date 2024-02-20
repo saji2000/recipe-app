bb339518a697786378aa9dedff5f71c3410188b1
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-20 14:27:14 -0500

    Authentication finished

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,16 +1,62 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
 import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+/**
+ * Registers a new user
+ * @param {Object} req.body - Request body
+ * @param {string} req.body.username - The username of the user.
+ * @param {string} req.body.password - The password of the user.
+ */
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
   const user = await UserModel.findOne({ username: username });
-  res.json(user);
+  if (user) {
+    return res.json({ message: "User already registered" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new UserModel({
+    username: username,
+    password: hashedPassword,
+  });
+
+  await newUser.save();
+
+  res.json({ message: "User registered successfully" });
 });
 
-router.post("/login", async (req, res) => {});
+/**
+ * Logins a user and creates a jwt token
+ * @param {Object} req.body - Request body
+ * @param {string} req.body.username - The username of the user.
+ * @param {string} req.body.password - The password of the user.
+ */
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username: username });
+
+  if (!user) {
+    return res.json({ message: "Username or password incorrect" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.json({ message: "Username or password incorrect" });
+  }
+
+  const token = jwt.sign({ id: user._id }, JWT_SECRET);
+
+  return res.json({ token, userId: user._id });
+});
 
 export { router as userRouter };
