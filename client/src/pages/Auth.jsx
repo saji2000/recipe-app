1d9f9026a9f6e1c9361eba092a459804b49a2355
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-25 11:24:51 -0500

    Register UI finished

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import { Button } from "@mui/material";
+import { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  return (
+    <div className="auth">
+      <Login />
+      <Register />
+    </div>
+  );
+};
+
+const Login = () => {
+  return <div></div>;
+};
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="atuh-container">
+      <form>
+        <h2>Register</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="Password">Password: </label>
+          <input
+            type="text"
+            id="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
