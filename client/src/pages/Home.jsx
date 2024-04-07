ee8c1dd0884bc7885825fb754b8762684eedfa21
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-06 22:38:22 -0400

    Can fetch recipes, praise God

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3003/recipes");
+        setRecipes(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(recipes);
+
   return <div>Home</div>;
 };
 
